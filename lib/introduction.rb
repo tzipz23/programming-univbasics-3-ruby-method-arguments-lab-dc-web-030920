d213f54b7b904d8ee4aa60fe98fe72a513f9bc28
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-20 00:57:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,18 +2,16 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction(ZIPZ)
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
-  introduction_with_language(ZIPZ, Ruby)
   
   def introduction_with_language_optional(name, language = "Ruby")
   end
   
-  introduction_with_language_optional(ZIPZ)
+  
   
 
 
